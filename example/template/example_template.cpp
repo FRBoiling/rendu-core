1981b785b5a9f18b38d0b376c84871e055580c71
fanrong <fanrong@h3d.com.cn>
2023-03-10 19:55:24 +0800

    Initial commit.
    例子

--- example/template/example_template.cpp
+++ example/template/example_template.cpp
@@ -6,7 +6,6 @@
 #include <map>
 #include <vector>
 
-void MergeContainer(std::map<int, std::set<int>> &container1, const std::map<int, std::set<int>> &container3);
 template<typename T>
 T max(T x, T y) {
   return x < y ? y : x;
@@ -17,19 +16,8 @@ int64_t max<int64_t>(int64_t x, int64_t y) // 加上一个尖括号并指定特�
 {
   return x < y ? x : y; // 返回较小值
 }
-void MergeContainer(std::map<int, std::set<int>> &container1, std::map<int, std::set<int>> &container3) {
-  for(auto it = container1.begin(); it!=container1.end(); ++it){
-    container3[it->first].insert(it->second.begin(),it->second.end());
-  }
-}
 
-class TestSortClass {
- public:
-      TestSortClass():m_key(0),m_value(0){}
- public:
-  int m_key;
-  int m_value;
-};
+
 
 
 int main() {
@@ -43,42 +31,6 @@ int main() {
   std::cout << ::max<int64_t>(10, 20) << std::endl;
 
   std::cout << "Hello Template" << std::endl;
-
-  std::map<int, std::set<int> > container1;
-  std::map<int, std::set<int> > container2;
-  std::map<int, std::set<int> > container3;
-  for (int i = 0; i < 2; ++i) {
-    for (int j = 0; j < 3; ++j) {
-      container1[i].insert(j);
-      container2[i].insert(j+3);
-    }
-  }
-  MergeContainer(container1, container3);
-  MergeContainer(container2, container3);
-
-
-  std::map<int,TestSortClass> temp_back_groups;
-  int key = 10;
-  int value = 1;
-  while (key > 0) {
-    TestSortClass item;
-    item.m_value = value;
-    item.m_key = key;
-    temp_back_groups[item.m_key] = item;
-    key--;
-    value++;
-  }
-
-  std::vector<TestSortClass> temp_vec;
-  for (auto it = temp_back_groups.begin(); it != temp_back_groups.end(); ++it) {
-    temp_vec.push_back(it->second);
-  }
-  std::sort(temp_vec.begin(), temp_vec.end(),
-            [](const TestSortClass& l, const TestSortClass& r) {
-              return l.m_value < r.m_value;
-            });
-
-  std::cout << "Hello container" << std::endl;
 
 }
 
