e2f8ca079d70c59fca25b5d903c0591ba575424f
Boil <free22858@live.com>
2023-03-24 23:33:29 +0800

    Initial commit.
    proto test

---
+++ example/protobuf/main.cpp
@@ -0,0 +1,12 @@
+/*
+* Created by boil on 2022/10/5.
+*/
+
+#include <google/protobuf/compiler/command_line_interface.h>
+#include <google/protobuf/compiler/cpp/cpp_generator.h>
+#include <google/protobuf/compiler/csharp/csharp_generator.h>
+
+// Must be included last.
+int main(int argc, char *argv[]) {
+
+}
