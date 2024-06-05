b910bd84e2f398d76a908566222e54ee6aa275e4
boil <FRee22858@live.com>
2024-06-05 17:38:49 +0800

    ✨ feat(base): 项目结构调整

--- rendu/base/base/container/concurrent/concurrent_queue.h
+++ rendu/base/base/container/concurrent/concurrent_queue.h
@@ -6,29 +6,13 @@
 #define RENDU_BASE_CONCURRENT_QUEUE_H
 
 #include "base_define.h"
-#include "boost/lockfree/queue.hpp"
 
 
 RD_NAMESPACE_BEGIN
 template<typename T>
 class ConcurrentQueue {
 private:
-  boost::lockfree::queue<T> queue_;
 
-public:
-  ConcurrentQueue() : queue_(128) {}// 初始化队列大小
-
-  void push(const T &value) {
-    queue_.push(value);
-  }
-
-  bool try_pop(T &popped_value) {
-    return queue_.pop(popped_value);
-  }
-
-  bool empty() const {
-    return queue_.empty();
-  }
 };
 
 RD_NAMESPACE_END
--- rendu/example/thread/thread_example.h
+++ rendu/example/thread/thread_example.h
@@ -17,8 +17,45 @@
 #include <iostream>
 #include <thread>
 
-void thread_main(){
+#include <iostream>
+#include <thread>
 
+
+#include <iostream>
+#include <thread>
+
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+// 子线程要执行的任务
+void task(const std::string& name) {
+  for(int i = 0; i < 5; ++i) {
+    // 模拟一些耗时操作
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "线程 " << name << " 执行中...\n";
+  }
+}
+
+int thread_main() {
+  // 创建两个子线程
+  std::thread thread1(task, "Thread 1");
+  std::thread thread2(task, "Thread 2");
+
+  // 主线程继续执行其它任务
+  for(int i = 0; i < 5; ++i) {
+    // 主线程也执行一些耗时操作
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "主线程执行中...\n";
+  }
+
+  // 等待两个子线程执行结束
+  thread1.join();
+  thread2.join();
+
+  std::cout << "所有线程执行完毕。\n";
+
+  return 0;
 }
 
 #endif//RENDU_RDTask_EXAMPLE_H
